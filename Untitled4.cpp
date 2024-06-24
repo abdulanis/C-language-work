a82f4c8093b89b99c0acca92ef67e56705c6b4e6
abdulanis <abdulanis.shaikh_mech22@pccoer.in>
2024-06-24 15:22:30 +0530

    Add files via upload

---
+++ Untitled4.cpp
@@ -0,0 +1,15 @@
+ //if else else if nested if
+//control statement
+
+//syntax of if block 
+//if(condition){}
+#include<stdio.h>
+int main(){
+	int data=1001;
+	if(data==1000){
+		printf("condition is true");
+	}
+	else{
+		printf("condition is false");
+	}
+}
